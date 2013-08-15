52d1da155932c0bd44958bb298dd315d562544cd
Joe Doherty <joe@pjuu.com>
2013-08-15 10:33:37 +0000

    Moved away from CherryPy for dev server to reduce dependencies

--- run.py
+++ run.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python
-import cherrypy
 from pjuu import app
 
-cherrypy.tree.mount(app, '/')
-cherrypy.config.update({'engine.autoreload_on': False})
-
 if __name__ == '__main__':
-    try:
-        server.start()
-    except KeyboardInterrupt:
-        server.stop()
+    app.run(host='0.0.0.0')
