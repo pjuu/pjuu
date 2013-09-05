8ca9f2a3d4cfeb8d6a04311158f74ca1cf8af0a3
Joe Doherty <joe@pjuu.com>
2013-09-05 10:20:16 +0000

    Lets actually delete run.py and crete_db.py this time

--- create_db.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-from pjuu import db
-
-print "Creating database and tables..."
-db.create_all()
-print "Complete"
--- run.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from pjuu import app
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
