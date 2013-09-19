e4686997eb82be5cf7e2934073570f9ef826f26f
Joe Doherty <joe@pjuu.com>
2013-09-19 12:47:23 +0000

    Updated settings_example.py so it can be deployed easier

--- pjuu/settings_example.py
+++ pjuu/settings_example.py
@@ -1,13 +1,21 @@
 # Pjuu example settings file
 # Please create a 'settings.py' from the template below
-
 DEBUG = True
 
 SECRET_KEY = 'Development Key'
 
-# SQL Alchemy
-SQLALCHEMY_DATABASE_URI = ''
+# Flas-SQLAlchemy
+SQLALCHEMY_DATABASE_URI = 'sqlite:///pjuu.db'
 
-# WTForms
+# Flask-Mail
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USE_TLS = False
+MAIL_USE_SSL = False
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+MAIL_DEFAULT_SENDER = 'Pjuu <noreply@pjuu.com>'
+
+# Flask-WTF
 CSRF_ENABLE = True
 CSRF_SESSION_KEY = SECRET_KEY
