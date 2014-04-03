cd8f07a4e0ca4e73473bba9f8057a49019428b78
Joe Doherty <joe@pjuu.com>
2014-04-03 08:56:18 +0100

    Updates settings_example.py for easy dev

--- pjuu/settings_example.py
+++ pjuu/settings_example.py
@@ -11,6 +11,10 @@ NOMAIL = False
 # Keep it secret, keep it safe
 SECRET_KEY = 'Development Key'
 
+# Redis settings (this is just the datastore, not sessions)
+REDIS_HOST = 'redis'
+REDIS_DB = 0
+
 # Flask-Mail
 MAIL_SERVER = 'localhost'
 MAIL_PORT = 25
@@ -24,9 +28,17 @@ MAIL_DEFAULT_SENDER = 'Pjuu <noreply@pjuu.com>'
 # Change this for extra security.
 CSRF_SESSION_KEY = SECRET_KEY
 
+# Recaptcha
+RECAPTCHA_USE_SSL = True
+RECAPTCHA_PUBLIC_KEY = ''
+RECAPTCHA_PRIVATE_KEY = ''
+RECAPTCHA_OPTIONS = {
+    'theme': 'white'
+}
+
 # Pagination
-FEED_ITEMS_PER_PAGE = 50
-PROFILE_ITEMS_PER_PAGE = 50
+FEED_ITEMS_PER_PAGE = 25
+PROFILE_ITEMS_PER_PAGE = 25
 
 # Signer Keys
 # Change these for extra security. Please see pjuu.auth.backend for details
@@ -34,3 +46,6 @@ TOKEN_KEY = SECRET_KEY
 SALT_ACTIVATE = 'ACTIVATE'
 SALT_FORGOT = 'FORGOT'
 SALT_EMAIL = 'EMAIL'
+
+# Logger file (this is for Warnings>)
+LOG_FILE = ''
