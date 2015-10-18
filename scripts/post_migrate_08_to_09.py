7ba07dbb0fc67ef94045cc625de019e01ad5e02b
Joe Doherty <joe@pjuu.com>
2015-10-18 10:31:38 +0100

    Flake test failure in script

--- scripts/post_migrate_08_to_09.py
+++ scripts/post_migrate_08_to_09.py
@@ -8,8 +8,12 @@
 """
 
 # Add Pjuu in the parent directory to the path
-import os, sys, inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(
+    os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
