23b2f31a008f9ceb08bc95a138e4528b478d21b3
Joe Doherty <joe@pjuu.com>
2016-01-13 12:52:29 +0000

    Hashtag search and search modifiers `#` and `@`.
    Hashtags will now show up in the search. If a user just wants to search
    for users on there own the search can be prefixed with `@`. If only
    hashtags are wanted then the prefix `#` can be used.
    
    Closes #137.

--- scripts/post_migrate_08_to_09.py
+++ scripts/post_migrate_08_to_09.py
@@ -17,10 +17,10 @@ currentdir = os.path.dirname(
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
-import pymongo
+import pymongo  # noqa
 
-from pjuu import create_app
-from pjuu.lib.parser import parse_post
+from pjuu import create_app  # noqa
+from pjuu.lib.parser import parse_post  #noqa
 
 
 m = pymongo.MongoClient(host='localhost')
