94ece84655183b6906402f721d2e5a7d4d5e9157
Joe Doherty <git@pjuu.com>
2017-01-14 19:23:56 +0000

    Mail was not delaying the send just deco task. (#316)
    * Mail was not delaying the send just deco task.
    
    * Flake issue (don't rush Joe)

--- pjuu/lib/mail.py
+++ pjuu/lib/mail.py
@@ -16,8 +16,15 @@ from pjuu import celery, mail
 
 def send_mail(subject, recipients, sender=None,
               text_body='', html_body=''):
-    """Sends e-mail via flask-mail"""
+    """Wrapper to send mail async over celery"""
+    send_async_mail.delay(subject, recipients, sender=sender,
+                          text_body=text_body, html_body=html_body)
 
+
+@celery.task()
+def send_async_mail(subject, recipients, sender=None,
+                    text_body='', html_body=''):
+    """Actually send a use an email"""
     # Set the default sender if one is not supplied
     if sender is None:  # pragma: no branch
         sender = app.config['MAIL_DEFAULT_SENDER']
@@ -28,9 +35,4 @@ def send_mail(subject, recipients, sender=None,
     msg.sender = sender
     msg.body = text_body
     msg.html = html_body
-    deliver_mail.delay(msg)
-
-
-@celery.task()
-def deliver_mail(msg):
     mail.send(msg)
