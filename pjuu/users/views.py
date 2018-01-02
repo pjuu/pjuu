25c5ffb6d77450e97a776fb026a552e8a4ab45c5
Ant Kenworthy <mcrmonkey@users.noreply.github.com>
2018-01-02 19:15:53 +0000

    Bump year in copyright statements (#336)

--- pjuu/users/views.py
+++ pjuu/users/views.py
@@ -3,7 +3,7 @@
 """Flask endpoints.
 
 :license: AGPL v3, see LICENSE for more details
-:copyright: 2014-2017 Joe Doherty
+:copyright: 2014-2018 Joe Doherty
 
 """
 
--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -3,7 +3,7 @@
 """Simple tests for auth tokens.
 
 :license: AGPL v3, see LICENSE for more details
-:copyright: 2014-2017 Joe Doherty
+:copyright: 2014-2018 Joe Doherty
 
 """
 
